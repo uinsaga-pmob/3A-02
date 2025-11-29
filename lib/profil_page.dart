d75f748b271f56d5faf942930019c9f86dae83c4
hidayatulka <hidayatulkarimah487@gmail.com>
2025-11-29 15:00:01 +0700

    saya menambahkan profil page di dart

---
+++ lib/profil_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProfilPage extends StatelessWidget {
+  const ProfilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.white, Color(0xFF5FB9E3)],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
