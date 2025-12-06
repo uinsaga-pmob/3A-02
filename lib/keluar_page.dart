abe33ebbd9a3e437890187e05e0c0097ecf26d48
khansanurulh-blip <khansanurulh@gmail.com>
2025-12-06 11:09:42 +0700

    Saya menambahkan keluar page dari profil page di dart

--- lib/keluar_page.dart
+++ lib/keluar_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:app_3a_02/profil_page.dart';
+
 
 class KeluarPage extends StatelessWidget{
   const KeluarPage({super.key});
@@ -16,6 +18,77 @@ class KeluarPage extends StatelessWidget{
               colors: [
                 Colors.white,
                 Color(0xFF5FB9E3),
+              ],
+            ),
+          ),
+
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Apakah kamu yakin ingin keluar?",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.blue,
+                  ),
+                ),
+
+                SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        SystemNavigator.pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 10,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(20),
+                        ),
+                      ),
+                      child: Text(
+                        "Ya",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                SizedBox(width: 24),
+                ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ProfilPage()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 10,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(20),
+                        ),
+                      ),
+                      child: Text(
+                        "Tidak",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ), 
+                  ],
+                ),
               ],
             ),
           ),
