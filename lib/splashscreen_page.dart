fb11d2b60d5ebbb864b2d5be223551891836a61d
hidayatulkarimah487-cp.cls <hidayatulkarimah487@gmail.com>
2026-06-03 10:55:28 +0700

    new splashscreen

--- lib/splashscreen_page.dart
+++ lib/splashscreen_page.dart
@@ -13,15 +13,18 @@ class _SplashPagesState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 5), () {
-      Navigator.pushReplacement(
-        context, 
-        MaterialPageRoute(builder: (_) => LoginPage()),
+
+    Timer(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (_) => LoginPage()),
         );
+      }
     });
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -31,15 +34,38 @@ class _SplashPagesState extends State<SplashPage> {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [Colors.white, Color(0xFF5FB9E3)],
-           ),
-        ),
-        child: Center(
-          child: Image.asset(
-            "assets/images/logo.png",
-            width: 300,
-            fit: BoxFit.contain,
+            colors: [Color.fromARGB(255, 73, 128, 211), Color(0xFF1B3A6F)],
           ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: -30,
+              left: -20,
+              child: Opacity(
+                opacity: 0.5,
+                child: Icon(
+                  Icons.local_florist,
+                  size: 180,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 40,
+              right: -20,
+              child: Opacity(
+                opacity: 0.5,
+                child: Icon(
+                  Icons.local_florist,
+                  size: 150,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            Center(child: Image.asset("assets/images/logo.png", width: 180)),
+          ],
         ),
       ),
     );
